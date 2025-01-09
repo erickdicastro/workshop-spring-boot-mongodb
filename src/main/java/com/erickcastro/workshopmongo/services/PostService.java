aeb78939ad376e1ffd191f1d59a470414fcb5a43
erick castro <erickcastro123@gmail.com>
2025-01-08 22:56:46 -0300

    Obtendo post por ID

---
+++ src/main/java/com/erickcastro/workshopmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.erickcastro.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erickcastro.workshopmongo.domain.Post;
+import com.erickcastro.workshopmongo.repository.PostRepository;
+import com.erickcastro.workshopmongo.services.exception.ObjectNoFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+	    return repo.findById(id)
+	    .orElseThrow(() -> new ObjectNoFoundException("Objeto não encontrado para o ID: " + id));
+	}
+}
