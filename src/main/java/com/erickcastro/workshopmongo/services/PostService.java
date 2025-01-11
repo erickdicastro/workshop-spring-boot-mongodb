65c2191193bcf484c9cd861cc463746128b29e86
erick castro <erickcastro123@gmail.com>
2025-01-11 16:42:06 -0300

    Consulta simples com query methods

--- src/main/java/com/erickcastro/workshopmongo/services/PostService.java
+++ src/main/java/com/erickcastro/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.erickcastro.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,9 @@ public class PostService {
 	public Post findById(String id) {
 	    return repo.findById(id)
 	    .orElseThrow(() -> new ObjectNoFoundException("Objeto não encontrado para o ID: " + id));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
